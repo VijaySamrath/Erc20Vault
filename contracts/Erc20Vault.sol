4475d793fe4dcc3c8bae6c913b9adb1f2c04fb36
vijay samrath <vijaysamrath462@gmail.com>
2022-11-16 11:19:36 +0530

    Initial Commit

--- contracts/Erc20Vault.sol
+++ contracts/Erc20Vault.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Vault is Ownable{
+contract Erc20Vault is Ownable{
     using SafeERC20 for IERC20;
 
     struct  Diary{
